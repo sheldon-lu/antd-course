6fa22bf8d1c1fdc511e795d56540b95183fcb973
lusheldon <shenneng.lu@aseit.cn>
2019-11-06 16:28:56 +0800

    first

---
+++ src/services/testPage.js
@@ -0,0 +1,22 @@
+import request from '../utils/request';
+
+// export function badges() {
+//   return request(`https://cdn.xgqfrms.xyz/json/badges.json`);
+// }
+
+export function shoppingWZ() {
+  return { data: { data: ['天猫', '京东', '苏宁'] } };
+}
+export function shoppingWZ2() {
+  // const date = new Date();
+  // const timer = date.getTime().toString();
+  // console.log(request('/mock/shopping?t=' + timer));
+  return request('/mock/shopping');
+}
+
+export function create(values) {
+  return request('/api/users', {
+    method: 'POST',
+    body: JSON.stringify(values),
+  });
+}
